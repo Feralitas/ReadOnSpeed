e71e18ca277e751b5a23490f617fc7f61b766087
Feralitas <Feralitas@yahoo.com>
2019-09-28 22:11:34 +0200

    improvements

--- main.py
+++ main.py
@@ -105,30 +105,65 @@ class TDE(Widget):  # Text display engine
 class ReadOnSpeedApp(App):
 
     def callbackWriteText(self, label):
-        self.textGen.callbackWriteText(label)
+        if self.StatusOfApp == 1:
+            self.textGen.callbackWriteText(label)
     
+    def getHandleOfThisWindow(self):
+        if self.handle == 0:
+            self.handle = win32gui.FindWindow(None, "ReadOnSpeedApp")
+        return self.handle
+
     def makeItTransparent(self, alpha):# Set alpha between 0 and 1. 0 no opacity, 1 invisible
         alpha = int((1-alpha) * 255)
-        handle = win32gui.FindWindow(None, "ReadOnSpeedApp")
-        win32gui.SetWindowLong(handle, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(handle, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)   # Make it a layered window
-        win32gui.SetLayeredWindowAttributes(handle, win32api.RGB(0, 0, 0), alpha, win32con.LWA_ALPHA)        # make it transparent (alpha between 0 and 255)
+        win32gui.SetWindowLong(self.getHandleOfThisWindow(), win32con.GWL_EXSTYLE, win32gui.GetWindowLong(self.getHandleOfThisWindow(), win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)   # Make it a layered window
+        win32gui.SetLayeredWindowAttributes(self.getHandleOfThisWindow(), win32api.RGB(0, 0, 0), alpha, win32con.LWA_ALPHA)        # make it transparent (alpha between 0 and 255)
     
     def makeItForeground(self):
-        handle = win32gui.FindWindow(None, "ReadOnSpeedApp")
         shell = win32com.client.Dispatch("WScript.Shell")
         shell.SendKeys('%')
-        win32gui.SetForegroundWindow(handle)
+        win32gui.SetForegroundWindow(self.getHandleOfThisWindow())
 
     def PositionToMouse(self):
-        handle = win32gui.FindWindow(None, "ReadOnSpeedApp")
         shell = win32com.client.Dispatch("WScript.Shell")
         shell.SendKeys('%')
         flags, hcursor, (x,y) = win32gui.GetCursorInfo()
-        win32gui.SetWindowPos(handle, win32con.HWND_TOP, x - 250, y - 210, 500, 200, win32con.SWP_SHOWWINDOW)
+        win32gui.SetWindowPos(self.getHandleOfThisWindow(), win32con.HWND_TOP, x - 250, y - 210, 500, 200, win32con.SWP_SHOWWINDOW)
         self.makeItForeground()
+    
+    def hibernate(self):
+        win32gui.ShowWindow(self.getHandleOfThisWindow(), 0)
+        self.StatusOfApp = 0
+
+    def startUp(self):
+        win32gui.ShowWindow(self.getHandleOfThisWindow(),1)
+        self.PositionToMouse()
+        self.makeItTransparent(.2)
+        self.StatusOfApp = 1
+
+    def waitForWakeUpCall(self):
+        if self.StatusOfApp == 0:
+            wakeUpKey = {
+           0x10: 'shift',
+           0x20: 'space'}
+            for i in range(1, 256):
+                if win32api.GetAsyncKeyState(i):
+                    if i in wakeUpKey:
+                        self.startUp()
+        if self.StatusOfApp == 1:
+            shutDownKey = {
+           0x10: 'shift',
+           0x20: 'space'}
+            for i in range(1, 256):
+                if win32api.GetAsyncKeyState(i):
+                    if i in shutDownKey:
+                        self.hibernate()
+
+
 
     def build(self):
         ##Experiment
+        self.handle = 0  #init
+        self.StatusOfApp = 0
         parent = MyBackground()
         ##Exp End
 
@@ -143,9 +178,8 @@ class ReadOnSpeedApp(App):
         parent.add_widget(self.textGen)
 
         Clock.schedule_interval(lambda dt: self.callbackWriteText(label), 0.001)
-        Clock.schedule_once(lambda dt: self.makeItTransparent(alpha=0.0), 0.1)
-        Clock.schedule_once(lambda dt: self.textGen.setToMiddle(), 0.2)
-        Clock.schedule_interval(lambda dt: self.PositionToMouse(),0.1)
+        Clock.schedule_once(lambda dt: self.hibernate(), 0.2) # initialiying the hibernate after start up
+        Clock.schedule_interval(lambda dt: self.waitForWakeUpCall(),0.1) #This loop is always listening for a wake up or suspend.
         # Get the window
         return parent
 
