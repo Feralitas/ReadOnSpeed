35b6537a0fdc087bec16312244cc9cc774ea9e80
Phaiax <phaiax-git@invisibletower.de>
2019-09-28 20:41:21 +0200

    listerer via thread

--- mouseInterfaces.py
+++ mouseInterfaces.py
@@ -5,20 +5,18 @@ import subprocess
 import time
 import json
 import sys
+import threading
 
-server_process = subprocess.Popen(["logi-devmon.exe"], stdout=subprocess.DEVNULL)
-time.sleep(1)
-print("Server started")
 
-time_message2 = time.time_ns()
+time_message = time.time_ns()
+velocity = 0
 
 def processEvents(message):
-    global time_message2
+    global time_message, velocity
     #print(f"{message}")
     message = json.loads(message)
     if message["path"] == "wheel" and message["success"]:
-        time_message1 = time.time_ns()
-        time_message2 = time_message1
+        time_message = time.time_ns()
         velocity = message['value']['delta'] * 1.0
         print(f"{message['value']['delta']: 5}, {velocity: 15.4}, {time_message1}", end="")
         print(f"{message['value']['hires']: 5}, {message['value']['periods']: 5}")
@@ -28,42 +26,59 @@ def processEvents(message):
     else:
         return True
 
-mouseUnitId = 0
-keyboardUnitId = 0
 
-logidevmon.list_devices()
-logtech_devices = logidevmon.LOGITECH_DEVICES
-if not isinstance(logtech_devices, list):
-    logtech_devices = [logtech_devices]
-print(logtech_devices)
 
-for device in logtech_devices:
-    if (device["type"] == "keyboard"):
-        keyboardUnitId = device['unitId']
+def mouse_event_listener_thread():
 
-    if (device["type"] == "mouse"):
-        mouseUnitId = device['unitId']
+    server_process = subprocess.Popen(["logi-devmon.exe"], stdout=subprocess.DEVNULL)
+    print("logi-devmon.exe", end="")
+    sys.stdout.flush()
+    time.sleep(1)
+    print(" started")
+    sys.stdout.flush()
 
-if mouseUnitId == 0:
-    print("No mouse device found.")
-    exit()
+    mouseUnitId = 0
+    keyboardUnitId = 0
 
-print("Get Device info")
-devinfo = logidevmon.get_device_info(mouseUnitId)
-print(devinfo)
+    logidevmon.list_devices()
+    logtech_devices = logidevmon.LOGITECH_DEVICES
+    if not isinstance(logtech_devices, list):
+        logtech_devices = [logtech_devices]
+    print(logtech_devices)
 
-if not devinfo["isConnected"]:
-    print("Mouse is not connected")
-    exit()
+    for device in logtech_devices:
+        if (device["type"] == "keyboard"):
+            keyboardUnitId = device['unitId']
 
-logidevmon.set_specialKey_config(mouseUnitId, 86, True)
-logidevmon.set_specialKey_config(mouseUnitId, 83, True)
-logidevmon.set_wheel_config(mouseUnitId, divert=True, hires=True, invert=False)
-#logidevmon.set_spyConfig(mouseUnitId, spyButtons=False, spyKeys=False, spyPointer=False, spyThumbWheel=False, spyWheel=True)
-logidevmon.read_events(processEvents)
+        if (device["type"] == "mouse"):
+            mouseUnitId = device['unitId']
 
-server_process.kill()
-server_process.wait(2)
-print(server_process.returncode)
+    if mouseUnitId == 0:
+        print("No mouse device found.")
+        exit()
 
-print("End")
+    print("Get Device info")
+    devinfo = logidevmon.get_device_info(mouseUnitId)
+    print(devinfo)
+    sys.stdout.flush()
+
+    if not devinfo["isConnected"]:
+        print("Mouse is not connected")
+        exit()
+
+    logidevmon.set_specialKey_config(mouseUnitId, 86, True)
+    logidevmon.set_specialKey_config(mouseUnitId, 83, True)
+    logidevmon.set_wheel_config(mouseUnitId, divert=True, hires=True, invert=False)
+    #logidevmon.set_spyConfig(mouseUnitId, spyButtons=False, spyKeys=False, spyPointer=False, spyThumbWheel=False, spyWheel=True)
+    logidevmon.read_events(processEvents)
+
+    server_process.kill()
+    server_process.wait(2)
+    print(server_process.returncode)
+
+    print("Mouse Event Listener Thread Ended")
+
+
+def start_mouse_event_listener_thread():
+    t = threading.Thread(target=mouse_event_listener_thread) 
+
